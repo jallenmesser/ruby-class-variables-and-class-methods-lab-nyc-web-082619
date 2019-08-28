39f94e8c411fbed397a78db9aa837b660a4ed4e8
Jallen Messersmith <jallenmessersmith@gmail.com>
2019-08-28 03:10:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,22 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
+  @@genres = [ ]
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre 
+    @@genre << genre
     @@count += 1
+  end
+  
+  def count
+    @@count
+  end
+  
+  def genre
+    
   end
   
 end
